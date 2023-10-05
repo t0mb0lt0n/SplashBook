0accecd18510775809c5af90501109577fe30006
TomBolton <goralexwizard@gmail.com>
2023-10-05 21:59:13 +0300

    app configuration

--- MainViewController.swift
+++ MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SplashBookAG
 //
 //  Created by Alexander on 05.10.2023.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+
+    // MARK: - Navigation
 
 
 }
-

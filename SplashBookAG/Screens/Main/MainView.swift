11dfa6a0c21987f0f875978d626d1b964025d9bb
TomBolton <goralexwizard@gmail.com>
2023-10-06 18:18:27 +0300

    main view configuration

--- SplashBookAG/Screens/Main/MainView.swift
+++ SplashBookAG/Screens/Main/MainView.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class MainView: UIView {
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView()
+        return collectionView
+    }()
 
 
     init() {
         super.init(frame: .zero)
+        
+       
+        
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")

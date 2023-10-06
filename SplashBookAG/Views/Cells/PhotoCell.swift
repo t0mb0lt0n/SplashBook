8ca52fc2acd1fee2c8fcc21301dd2ce79119b52d
TomBolton <goralexwizard@gmail.com>
2023-10-06 19:37:15 +0300

    collectionView setup

--- SplashBookAG/Views/Cells/PhotoCell.swift
+++ SplashBookAG/Views/Cells/PhotoCell.swift
@@ -12,10 +12,26 @@ class PhotoCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super .init(frame: frame)
+        setup()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        self.contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        self.clipsToBounds = true
+        
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        
+        ])
     }
     
 

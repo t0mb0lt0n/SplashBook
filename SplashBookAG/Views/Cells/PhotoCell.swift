c2e04af013e169241d9b8b116d396554805d12f5
TomBolton <goralexwizard@gmail.com>
2023-10-06 19:31:46 +0300

    collectionView setup

--- SplashBookAG/Views/Cells/PhotoCell.swift
+++ SplashBookAG/Views/Cells/PhotoCell.swift
@@ -8,5 +8,15 @@
 import UIKit
 
 class PhotoCell: UICollectionViewCell {
+    let imageView = UIImageView()
     
+    override init(frame: CGRect) {
+        super .init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
 }

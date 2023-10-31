524077b38d7b7c47e82d9b491308dc5f622bdfba
TomBolton <goralexwizard@gmail.com>
2023-10-31 21:41:38 +0300

    TabBar configuration

--- SplashBookAG/Views/TabBars/MainTabBar.swift
+++ SplashBookAG/Views/TabBars/MainTabBar.swift
@@ -9,8 +9,26 @@ import Foundation
 import UIKit
 
 final class MainTabBar: UITabBarController {
+    private let vcList: [UIViewController]
+    
+    init(with list: [UIViewController] ) {
+        super.init(nibName: nil, bundle: nil)
+        self.vcList = list
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBar()
+        //viewControllers =
     }
+    
+    private func setupTabBar() {
+        
+    }
+    
+    //private func
 }

4b41f0b7cd1301198c12f6c7e616f4228651e5de
TomBolton <goralexwizard@gmail.com>
2023-11-02 23:09:32 +0300

    cell additionals configuration

--- SplashBookAG/Views/Cells/PhotoCell.swift
+++ SplashBookAG/Views/Cells/PhotoCell.swift
@@ -10,9 +10,6 @@ import UIKit
 class PhotoCell: UICollectionViewCell {
     let imageView: UIImageView = {
         var imageView = UIImageView()
-        //imageView.contentMode = .scaleAspectFit
-        //imageView.layer.cornerRadius = 20
-        //imageView.clipsToBounds = true
         return imageView
     }()
     
@@ -27,21 +24,14 @@ class PhotoCell: UICollectionViewCell {
     }
     
     private func setupCell() {
-        //imageView.translatesAutoresizingMaskIntoConstraints = false
         self.contentView.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        //imageView.translatesAutoresizingMaskIntoConstraints = false
-        //imageView.contentMode = .scaleAspectFill
-        //imageView.backgroundColor = .black
-        //layer.cornerRadius = 20
         backgroundColor = .green
-        //contentMode = .scaleAspectFill
-        //self.clipsToBounds = true
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, co),
             imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
             imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),

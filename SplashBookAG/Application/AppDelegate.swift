4e6b03872950a06ff058c648a0da4443cf0a5d4c
TomBolton <goralexwizard@gmail.com>
2023-11-07 22:18:56 +0300

    cell additionals configuration

--- SplashBookAG/Application/AppDelegate.swift
+++ SplashBookAG/Application/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
-        window?.rootViewController = MainTabBarController(with: [MainViewController.init(viewModel: .init())])
+        window?.rootViewController = MainTabBarController(with: [MainViewController.init(viewModel: .init(service: ImageService()))])
         window?.overrideUserInterfaceStyle = .dark
         window?.makeKeyAndVisible()
         return true

2b3fe9f9c45eabe7b7e90a481212ad368bf90739
TomBolton <goralexwizard@gmail.com>
2023-11-16 23:17:26 +0300

    minor improvements

--- SplashBookAG/NetworkLayer/ErrorHandle.swift
+++ SplashBookAG/NetworkLayer/ErrorHandle.swift
@@ -8,29 +8,3 @@
 import Foundation
 
 
-enum MachineErrors: Error {
-    case missingName
-    case priceFail(coinsNeeded: Int)
-    case outOfStock
-}
-
-struct Item2 {
-    var price: Int
-    var count : Int
-}
-
-class VendingBar {
-    var items = [
-        "Twix": Item2(price: 10, count: 2),
-        "Nuts": Item2(price: 3, count: 3),
-        "Cola": Item2(price: 9, count: 100)
-    ]
-    
-    private var deposit: Int = 0
-    
-    func addCoins(_ coins: Int) {
-        deposit += coins
-    }
-    
-    func getItem(named itemName: String, count: Int) throws {
-  

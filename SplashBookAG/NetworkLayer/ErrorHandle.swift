0f8a1c1e08f289779d838cceed082df092259767
TomBolton <goralexwizard@gmail.com>
2023-11-17 21:50:33 +0300

    minor improvements

--- SplashBookAG/NetworkLayer/ErrorHandle.swift
+++ SplashBookAG/NetworkLayer/ErrorHandle.swift
@@ -13,24 +13,41 @@ enum HttpError: Error {
     case badrequest400
 }
 
-
 class NetworkManager {
     func fetchData(_ data: String?) throws -> Int{
-        guard !data!.isEmpty else {
+        guard let data, !data.isEmpty else {
+            throw HttpError.serverError500
+        }
+        
+        guard !data.isEmpty else {
             throw HttpError.notFound404
         }
         
-        guard data == "data" else {
+        guard data != "data" else {
             throw HttpError.badrequest400
-        }
-        
-        guard let data else {
-            throw HttpError.serverError500
         }
         print(data)
         return 0
     }
 }
+
+let manager = NetworkManager()
+
+do {
+    try manager.fetchData("passed")
+} catch let error as HttpError {
+    switch error {
+    case .serverError500:
+        print("server error")
+    case .notFound404:
+        print("server not found")
+    case .badrequest400:
+        print("bad request")
+    }
+}
+
+
+
 
 let manager = NetworkManager()
 

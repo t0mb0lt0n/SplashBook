2d3622c68c671cc157807bfd7057d9c869030a4c
TomBolton <goralexwizard@gmail.com>
2023-11-27 20:46:08 +0300

    http request confugure

--- SplashBookAG/NetworkLayer/HTTPRequest.swift
+++ SplashBookAG/NetworkLayer/HTTPRequest.swift
@@ -18,15 +18,16 @@ struct HTTPRequest {
         server: String,
         endPoint: String,
         method: HTTPMethod,
-        URL: String,
         headers: HTTPHeaders? = nil,
         parameters: HTTPParameters? = nil
     ) throws -> URLRequest {
-        guard let url = try composeURL(with: tunnel, server: server, endPoint: endPoint) else {
+        guard let url = composeURL(with: tunnel, server: server, endPoint: endPoint) else {
             throw HTTPNetworkError.urlComposingError
         }
         print("passed")
-        let request = URLRequest(url: url)
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        request.
         return request
     }
     
@@ -34,34 +35,32 @@ struct HTTPRequest {
         with tunnel: String,
         server: String,
         endPoint: String
-    ) throws -> URL? {
-        guard let composedURL = URL(string: tunnel + server + endPoint) else {
-            throw HTTPNetworkError.urlComposingError
-        }
+    ) -> URL? {
+        let composedURL = URL(string: tunnel + server + endPoint)
         print(composedURL)
         return composedURL
     }
     
-//    static func configureHTTPRequest(
-//      from path: String,
-//      baseUrl: String,
-//      parameters: HTTPParameters? = nil,
-//      headers: HTTPHeaders? = nil,
-//      and method: HTTPMethod
-//    ) throws -> URLRequest {
-//      guard let url = URL(string: baseUrl + path) else {
-//        throw HTTPNetworkError.missingURL
-//
-//      }
-//      var request = URLRequest(
-//        url: url, cachePolicy: .reloadIgnoringLocalCacheData,
-//        timeoutInterval: timeoutInterval
-//      )
-//      request.httpMethod = method.rawValue
-//      try configureParametersAndHeaders(
-//        parameters: parameters,
-//        headers: headers, request: &request
-//      )
-//      return request
-//    }
+    static func configureHTTPRequest(
+      from path: String,
+      baseUrl: String,
+      parameters: HTTPParameters? = nil,
+      headers: HTTPHeaders? = nil,
+      and method: HTTPMethod
+    ) throws -> URLRequest {
+      guard let url = URL(string: baseUrl + path) else {
+        throw HTTPNetworkError.missingURL
+
+      }
+      var request = URLRequest(
+        url: url, cachePolicy: .reloadIgnoringLocalCacheData,
+        timeoutInterval: timeoutInterval
+      )
+      request.httpMethod = method.rawValue
+      try configureParametersAndHeaders(
+        parameters: parameters,
+        headers: headers, request: &request
+      )
+      return request
+    }
 }
---
+++ SplashBookAG/NetworkLayer/URLComposer.swift
@@ -0,0 +1,8 @@
+//
+//  URLComposer.swift
+//  SplashBookAG
+//
+//  Created by Alexander on 27.11.2023.
+//
+
+import Foundation

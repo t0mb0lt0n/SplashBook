a168f143bf345f99540556c48fca9b5f3a91592c
TomBolton <goralexwizard@gmail.com>
2023-11-27 18:21:41 +0300

    http request confugure

--- SplashBookAG/NetworkLayer/HTTPNetworkError.swift
+++ SplashBookAG/NetworkLayer/HTTPNetworkError.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+enum HTTPNetworkError: String, Error {
+    case badRequest400 = "HTTP Error 400 - Bad Request"
+    case requestTimeout408 = "HTTP Error 408 - Request Timeout"
+    case notFoundClient404 = "HTTP Error 404 - Not Found (Client)"
+    case methodNotAllowed405 = "HTTP Error 405 - Method Not Allowed"
+    case internalServerError500 = "HTTP Error 500 - Internal Server Error"
+    
+    
+}

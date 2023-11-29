e83379411a5b52694f0445dd60b1e31044b8fdb6
TomBolton <goralexwizard@gmail.com>
2023-11-29 17:20:53 +0300

    URL Encoder impl

--- SplashBookAG/NetworkLayer/URLEncoder.swift
+++ SplashBookAG/NetworkLayer/URLEncoder.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 struct URLEncoder {
-    sta
+    static func encodeURLParameters(
+        from urlRequest: inout URLRequest,
+        with parameters: HTTPParameters
+    ) throws {
+        if parameters == nil { return }
+        guard let url = urlRequest.url else {
+            throw NetworkFailure.URLEncoderError.missedURL
+        }
+        
+    }
 }

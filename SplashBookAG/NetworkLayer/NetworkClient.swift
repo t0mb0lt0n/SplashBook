4fe43fced11ea4b532269c4377bde2b5cc9d69d8
TomBolton <goralexwizard@gmail.com>
2023-12-01 21:22:19 +0300

    NetworkClient cfg

---
+++ SplashBookAG/NetworkLayer/NetworkClient.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkClient.swift
+//  SplashBookAG
+//
+//  Created by Alexander on 01.12.2023.
+//
+
+import Foundation
+
+class NetworkClient {
+    private lazy var session: URLSession
+    
+    init(session: URLSes
+    
+}

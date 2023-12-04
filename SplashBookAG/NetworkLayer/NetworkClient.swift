b46c694c32a5fd674398fab8b18050d0983d0238
TomBolton <goralexwizard@gmail.com>
2023-12-04 19:26:00 +0300

    NetworkClient cfg

--- SplashBookAG/NetworkLayer/NetworkClient.swift
+++ SplashBookAG/NetworkLayer/NetworkClient.swift
@@ -7,28 +7,3 @@
 
 import Foundation
 
-final class NetworkClient {
-    private var session: URLSession
-    
-    init(session: URLSession) {
-        self.session = session
-    }
-    
-    func downloadData<T: Decodable>(
-        scheme: String,
-        host: String,
-        path: String,
-        paremeters: HTTPParameters,
-        completion: @escaping (Result<T, Error>) -> Void
-    ) {
-        
-        
-    }
-    
-    private func execute() {
-        
-    }
-    
-    
-    
-}

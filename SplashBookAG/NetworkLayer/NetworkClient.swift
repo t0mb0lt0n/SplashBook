07746f8a5c901743bff466e65777e94108c7d3f6
TomBolton <goralexwizard@gmail.com>
2023-12-04 19:36:06 +0300

    NetworkClient cfg

--- SplashBookAG/NetworkLayer/NetworkClient.swift
+++ SplashBookAG/NetworkLayer/NetworkClient.swift
@@ -7,3 +7,39 @@
 
 import Foundation
 
+final class NetworkClient {
+    private var session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func downloadData<T: Decodable>(
+        scheme: String,
+        host: String,
+        path: String,
+        parameters: HTTPParameters,
+        comletion: @escaping (Result<T, Error>) -> Void
+    ) {
+        do {
+            let request = try HTTPRequest.configureURLRequest(
+                with: scheme,
+                host: host,
+                path: path,
+                method: .get
+            )
+            
+        } catch {
+            comletion(.failure(NetworkFailure.HTTPError.badRequest400))
+        }
+        
+    }
+    
+    
+    
+    func execute() {
+        
+    }
+    
+    
+}

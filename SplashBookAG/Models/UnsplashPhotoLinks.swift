69d5b7883a768d36c3ab307bd73c3c3e147dfd01
TomBolton <goralexwizard@gmail.com>
2023-12-08 18:37:17 +0300

    models configuration

--- SplashBookAG/Models/UnsplashPhotoLinks.swift
+++ SplashBookAG/Models/UnsplashPhotoLinks.swift
@@ -7,9 +7,30 @@
 
 import Foundation
 
-struct UnsplashPhotoLinks: Decodable {
+struct UnsplashPhotoLinks: deco {
     let selfLink: String
     let html: String
     let download: String
     let downloadLocation: String
+    
+    enum CodingKeys: String, CodingKey {
+        case selfLink = "self"
+        case html
+        case download
+        case downloadLocation
+    }
+}
+
+struct Post: Codable {
+    let userId: Int
+    let postId: Int
+    let title: String
+    let body: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userId
+        case postId = "id"
+        case title
+        case body
+    }
 }

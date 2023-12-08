a81198b3b76814e0f58a173b4f0ed6be853062eb
TomBolton <goralexwizard@gmail.com>
2023-12-08 18:13:34 +0300

    models configuration

--- SplashBookAG/Models/UnsplashUser.swift
+++ SplashBookAG/Models/UnsplashUser.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct UnsplashUser: Decodable {
+    let id: String
+    let username: String
+    let name: String
+    let firstName: String
+    let lastName: String
+    let twitterUsername: String
+    let portfolioURL: String
+    
+}

5dcd5d2649f65e2d9a07acce60fa183e3d24c36f
TomBolton <goralexwizard@gmail.com>
2023-12-12 22:21:12 +0300

    success content representation

--- SplashBookAG/Views/Cells/PhotoCell.swift
+++ SplashBookAG/Views/Cells/PhotoCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class PhotoCell: UICollectionViewCell {
     private let imageView: UIImageView = {
         let imageView = UIImageView()
-        //imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         return imageView
     }()
     

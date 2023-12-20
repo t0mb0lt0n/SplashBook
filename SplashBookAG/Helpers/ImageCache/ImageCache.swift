2d26d85ba5271b1b93607d53ac506bae57b3a878
TomBolton <goralexwizard@gmail.com>
2023-12-20 18:44:38 +0300

    Image Cache configuration

--- SplashBookAG/Helpers/ImageCache/ImageCache.swift
+++ SplashBookAG/Helpers/ImageCache/ImageCache.swift
@@ -5,4 +5,18 @@
 //  Created by Alexander on 20.12.2023.
 //
 
-import Foundation
+import UIKit
+
+final class ImageCache {
+    private let cache = NSCache<NSString, UIImage>()
+    
+    private init() { }
+    
+    func image(forKey key: String) -> UIImage? {
+        self.cache.object(forKey: key as NSString)
+    }
+    
+    func save(image: UIImage, forKey key: String) {
+        self.cache.setObject(image, forKey: key as NSString)
+    }
+}

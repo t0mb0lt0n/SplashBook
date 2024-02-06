480c19863f91e8c4ff8e089fb9e2ae6feddbcde3
TomBolton <goralexwizard@gmail.com>
2024-02-06 21:43:37 +0300

    download impr

--- SplashBookAG/Screens/Main/MainViewModel.swift
+++ SplashBookAG/Screens/Main/MainViewModel.swift
@@ -47,10 +47,10 @@ final class MainViewModel {
             
             switch result {
             case .success(let photos):
-                self.photos = photos.results
+                self?.photos = photos.results
                 //self.reloadClosure?()
                 print("Number of downloaded images = \(self.photos.count)")
-                self.currentPage += 1
+                self?.currentPage += 1
             case .failure(let error):
                 print(error.localizedDescription)
             }

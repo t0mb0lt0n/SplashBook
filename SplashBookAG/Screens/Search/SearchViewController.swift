6f975242791ff6fa8dcdf1108879d5da8a7b3b45
TomBolton <goralexwizard@gmail.com>
2024-02-19 21:30:50 +0300

    search view cfg

---
+++ SplashBookAG/Screens/Search/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  SplashBookAG
+//
+//  Created by Alexander on 19.02.2024.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

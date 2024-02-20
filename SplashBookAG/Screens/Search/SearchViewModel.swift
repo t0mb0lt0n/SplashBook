2efb0064a74df4529c64326d5b79d8ccb082a6fc
TomBolton <goralexwizard@gmail.com>
2024-02-20 21:17:44 +0300

    search view cfg

--- SplashBookAG/Screens/Search/SearchViewModel.swift
+++ SplashBookAG/Screens/Search/SearchViewModel.swift
@@ -9,11 +9,7 @@ import UIKit
 
 class SearchViewModel: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+   
     // MARK: - Navigation
 
 

5b46f94dbbe8316f37ed413eca4e03d7645d6c8d
TomBolton <goralexwizard@gmail.com>
2024-02-20 21:35:12 +0300

    search view cfg

--- SplashBookAG/Screens/Search/SearchView.swift
+++ SplashBookAG/Screens/Search/SearchView.swift
@@ -12,4 +12,13 @@ final class SearchView: UIView {
         let searchBar = UISearchBar()
         return searchBar
     }()
+    
+    init() {
+        super .init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }

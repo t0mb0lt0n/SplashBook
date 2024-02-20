a81899c8b1a215d37787d04d5dab5600aab15424
TomBolton <goralexwizard@gmail.com>
2024-02-20 21:56:47 +0300

    search view cfg

--- SplashBookAG/Screens/Search/SearchViewController.swift
+++ SplashBookAG/Screens/Search/SearchViewController.swift
@@ -13,6 +13,8 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view = SearchView()
+        title = "Search photo"
     }
+    
     // MARK: - Navigation
 }

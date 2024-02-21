832f6c52457772f2bc397233b7b08c6171e20f61
TomBolton <goralexwizard@gmail.com>
2024-02-21 20:01:56 +0300

    search view cfg

---
+++ SplashBookAG/Helpers/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController+Extension.swift
+//  SplashBookAG
+//
+//  Created by Alexander on 21.02.2024.
+//
+
+import UIKit
+
+class UIViewController_Extension: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

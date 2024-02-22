facafc67de458af6f061296b49b59f80abee7c05
TomBolton <goralexwizard@gmail.com>
2024-02-22 20:00:51 +0300

    search view cfg

--- SplashBookAG/Views/TabBars/MainTabBarController.swift
+++ SplashBookAG/Views/TabBars/MainTabBarController.swift
@@ -8,26 +8,19 @@
 import UIKit
 
 final class MainTabBarController: UITabBarController {
-    var initialVCList: [UIViewController]
+    private var initialVCList: [UIViewController]
     
-    init(with vcList: [UIViewController]) {
-        initialVCList = vcList
+    init(with viewControllers: [UIViewController]) {
+        initialVCList = viewControllers
         super.init(nibName: nil, bundle: nil)
-        //viewControllers = vcList
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func loadView() {
-//        guard let unwrappedVCList = viewControllers else { return }
-//        setupTabBar(with: unwrappedVCList)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //setupTabBar(with: unwrappedVCList)
         setupTabBar()
         setupTabBarStyle(
             backgroundColor: .systemBackground,
@@ -38,7 +31,6 @@ final class MainTabBarController: UITabBarController {
     
     private func setupTabBar() {
         var navigationControllers = [UINavigationController]()
-       // guard let unwrappe
         initialVCList.forEach { vc in
             let navController = UINavigationController(rootViewController: vc)
             navigationControllers.append(navController)
